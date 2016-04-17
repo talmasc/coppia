daf6e4c5f6dc822cc8deea46da05059d6dc948be
talmasc <eric.l.hillenbrand@gmail.com>
2016-04-16 18:18:49 -0700

    updated to add more fields in model

--- app/models/idea.js
+++ app/models/idea.js
@@ -3,7 +3,11 @@ var Schema = mongoose.Schema;
 
 var IdeaSchema = new Schema({
   title: {type: String, trim: true},
-  notes: {type: String, trim: true}
+  notes: {type: String, trim: true},
+  created_by: String,
+  created_date: Date
+  updated_by: String,
+  updated_date: Date
 });
 
 module.exports = mongoose.model('idea', IdeaSchema);
